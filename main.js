49f2bdbac7bf71f0826ecda117b948369f02b6cd
Ahmed Hagag <93779010+i7ego@users.noreply.github.com>
2023-03-28 00:12:27 +0200

    update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var i=0,text;
+//let name = prompt("Enter your lover name : ")
 text = `
-My sweet OLA,
+My sweet Ola,
 
 `
 
@@ -14,6 +15,7 @@ function typing(){
 typing();
 
 var l=0,text1;
+//let nam1 = prompt("Enter your name:")
 text1 = `
 You are the most amazing person I have ever met. You fill my life with happiness, laughter, and peace. You are my best friend, my soulmate, and my lover. You are the only one who can make me feel this way. I love you more than words can express.
 
