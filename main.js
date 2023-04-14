7d0b4b1a9282bf1e51ec26f940bca31814121d53
Ahmed Hagag <93779010+i7ego@users.noreply.github.com>
2023-04-14 07:02:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var i=0,text;
-//let name = prompt("Enter your lover name : ")
+let name = prompt("Enter your lover name : ")
 text = `
-My sweet Ola,
+My sweet ${nam},
 
 `
 
@@ -15,12 +15,12 @@ function typing(){
 typing();
 
 var l=0,text1;
-//let nam1 = prompt("Enter your name:")
+let nam1 = prompt("Enter your name:")
 text1 = `
 You are the most amazing person I have ever met. You fill my life with happiness, laughter, and peace. You are my best friend, my soulmate, and my lover. You are the only one who can make me feel this way. I love you more than words can express.
 
 Always yours, 
- Ahmed Hagag`
+ ${nam1}`
 function typing1(){
 	if(l<text1.length){
 		document.getElementById("head1").innerHTML += text1.charAt(l);
